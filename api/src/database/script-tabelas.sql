8f68dedd6e0d3744174cd13adf1afe6bffee1571
Guigon14 <guilherme.gsantos@sptech.school>
2023-05-09 13:00:42 -0300

    site

---
+++ api/src/database/script-tabelas.sql
@@ -0,0 +1,51 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/*
+comandos para mysql - banco local - ambiente de desenvolvimento
+*/
+
+CREATE DATABASE THAOP;
+
+USE THAOP;
+
+CREATE TABLE usuario (
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50)
+);
+
+CREATE TABLE quiz(
+	idQuiz INT PRIMARY KEY AUTO_INCREMENT,
+	resultado VARCHAR(45),
+	fkUsuario INT,
+	constraint FK_Usuario foreign key (idUsuario) 
+		references usuario (idUsuario)
+);
+/*
+comando para sql server - banco remoto - ambiente de produção
+*/
+
+-- CREATE TABLE usuario (
+-- 	id INT PRIMARY KEY IDENTITY(1,1),
+-- 	nome VARCHAR(50),
+-- 	email VARCHAR(50),
+-- 	senha VARCHAR(50),
+-- );
+
+-- /*
+-- comandos para criar usuário em banco de dados azure, sqlserver,
+-- com permissão de insert + update + delete + select
+-- */
+
+-- CREATE USER [usuarioParaAPIWebDataViz_datawriter_datareader]
+-- WITH PASSWORD = '#Gf_senhaParaAPIWebDataViz',
+-- DEFAULT_SCHEMA = dbo;
+
+-- EXEC sys.sp_addrolemember @rolename = N'db_datawriter',
+-- @membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
+
+-- EXEC sys.sp_addrolemember @rolename = N'db_datareader',
+-- @membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
